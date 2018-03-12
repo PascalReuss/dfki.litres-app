f93c493a70a5fb125c805619e13736da7d99d276
oli <oliver.daniel.berg@gmail.com>
2018-03-12 10:06:53 +0100

    basic form for sre setup (no functionality yet)

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('index', {
-      _title_: 'Boilerplate Bootstrap-NodeJS-Express App',
-      users: []
-    });
+  res.render('index', {
+    _title_: 'Boilerplate Bootstrap-NodeJS-Express App',
+    users: []
+  });
+});
+
+router.get('/sre', function(req, res) {
+  // TODO: load latest SRE and prefill fields (? ... this would make SREs not have a unique ts)
+  res.render('sre', {});
 });
 
 module.exports = router;
