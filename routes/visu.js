ba4c91ae8f8c0090d48847ff09f57590d4e8a4ce
oli <oliver.daniel.berg@gmail.com>
2018-04-19 13:29:15 +0200

    visu index showing item-progress; only content visible, no logic/linking yet

--- routes/visu.js
+++ routes/visu.js
@@ -3,6 +3,27 @@ var router  = express.Router();
 
 module.exports = function(dataAccess) {
 
+    router.get('/', function(req, res) {
+        dataAccess.findAllIn('sources').done(function(srcs) {
+            dataAccess.findAllIn('sres').done(function(sres) {
+                dataAccess.findAllIn('queries').done(function(queries) {
+                    dataAccess.findAllIn('processes').done(function(processes) {
+                        dataAccess.findAllIn('results').done(function(results) {
+                            return res.render('visu/index', {
+                                title: 'Visualization',
+                                sources: srcs,
+                                sres: sres,
+                                queries: queries,
+                                processes: processes,
+                                results: results
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+  
     router.get('/sources', function(req, res) {
         dataAccess.findAllIn('sources').done(function(srcs) {
             res.render('visu/sources', {
@@ -11,6 +32,6 @@ module.exports = function(dataAccess) {
             });
         });
     });
-  
+
   return router;
 };
